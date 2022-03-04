b0ac52048757d26528365b94f0f3322399de916b
赵玉 <yu.zhao@linkedcare.cn>
2022-03-04 16:49:35 +0800

    feat: init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="btn btn-primary">
+        Notifications <span className="badge bg-secondary">4</span>
+      </button>
+      <h1>Example heading <span className="badge bg-secondary">New</span></h1>
+      <h2>Example heading <span className="badge bg-secondary">New</span></h2>
+      <h3>Example heading <span className="badge bg-secondary">New</span></h3>
+      <h4>Example heading <span className="badge bg-secondary">New</span></h4>
+      <h5>Example heading <span className="badge bg-secondary">New</span></h5>
+      <h6>Example heading <span className="badge bg-secondary">New</span></h6>
+    </div>
+  );
+}
+
+export default App;
